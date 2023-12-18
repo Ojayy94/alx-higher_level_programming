77721ac3438067625d74affe846ac7c17b7a160e
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-12-18 14:21:16 -0800

    task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """Returns the division of a by b."""
+    try:
+        i = a / b
+    except (TypeError, ZeroDivisionError):
+        i = None
+    finally:
+        print("Inside result: {}".format(i))
+    return i
