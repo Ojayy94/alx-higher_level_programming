0bc9dbae7dcd00862fac08c97fb8764d7d70defa
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-12-18 14:08:17 -0800

    task 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    a = 0
+    lst = 0
 
     try:
-        for lst in my_list:
+        for i in my_list:
             if lst < x:
                 print('{}'.format(my_list[lst]), end='')
                 lst += 1
