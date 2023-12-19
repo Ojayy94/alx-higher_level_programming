d14368c28737be129f11de23248405943c57f2c0
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2023-12-19 14:41:36 -0800

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/bin/usr/python3
+
+"""Write a class Square that defines a square by: (based on 0-square.py)"""
+
+class Square:
+
+    """Represent square"""
+
+    def __init__(self, size):
+
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        
+        self.__size = size
