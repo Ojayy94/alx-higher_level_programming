6f78f16b2d28b81fe6c5ff9b35c1b491266b379e
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-01-04 14:53:48 -0800

    Task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -31,7 +31,7 @@ class Rectangle:
 
             if value < 0:
                 raise ValueError("width must be >= 0")
-                self.__width = value
+            self.__width = value
 
         @property
         def height(self):
@@ -48,4 +48,4 @@ class Rectangle:
 
             if value < 0:
                 raise ValueError("height must be >= 0")
-                self.__height = value
+            self.__height = value
