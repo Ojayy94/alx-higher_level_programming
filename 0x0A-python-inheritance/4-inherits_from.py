e2149f0736768070ad50937439c89450925bd58d
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-01-08 06:53:46 -0800

    Task

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,7 +8,5 @@ def inherits_from(obj, a_class):
        otherwise False
     """
 
-    if issubclass(obj, a_class) and obj != a_class:
+    if isinstance(obj, a_class) and issubclass(a_class, obj.__class) is False:
         return True
-    else:
-        return False
