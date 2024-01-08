0025d79698db516348373931ddad9e90e49be5f7
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-01-08 07:10:27 -0800

    Task

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""empty class"""
+
+
+class BaseGeometry:
+    """BaseGeometry empty class"""
+    def area(self):
+        """raise Exception"""
+        raise Exception(area() is not implemented)
+   
