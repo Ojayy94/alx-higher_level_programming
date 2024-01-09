daf19588b7f045486d819879115fd95b24b0ae68
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-01-09 14:45:18 -0800

    json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """Add argument and save to file"""
 import sys
-import os
 
 
 if __name__ == "__main__":
     save_to_json_file = __import__(5-save_to_json_file.py).save_to_json_file
     load_from_json_file = __import__\
                          (6-load_from_json_file.py).load_from_json_file
+
     try:
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
