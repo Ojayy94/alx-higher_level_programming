7350d8689820f3829e68826f11917814b813cfcb
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-01-09 07:34:19 -0800

    student instance representation

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Write a class"""
+
+
+class Student:
+    """student attributes"""
+    def __init__(self, first_name, last_name, age):
+        """create an object instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns the dictionary description with simple data structure"""
+        cls_d = self.__dict__
+        my_d = dict()
+
+        if type(attrs) is list:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return cls_d
+
+                if attr in cls_d:
+                    my_d[attr] = cls_d[attr]
+
+            return my_d
+
+        return self.__dict__
