fe2555d5ece13d01db914c2a927a192e6ea45a41
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-02-14 16:03:53 -0800

    using select to list and join items

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -5,5 +5,5 @@
 -- +tv_shows.title and tv_show_genres.genre_id
 -- You can use only one SELECT statement
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows JOIN tv_show_genres.id = tv_show_genres.show_id
+FROM tv_shows JOIN tv_show_genres on tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
