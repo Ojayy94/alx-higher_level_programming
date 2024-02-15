fa688452981e27e65758d733623f9503b3f20e48
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-02-14 16:11:55 -0800

    shows sort

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -6,5 +6,5 @@
 -- If a show doesn’t have a genre, display NULL
 -- You can use only one SELECT statement
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows LEFT JOIN v_show_genres ON tv_shows.id = tv_show_genres.show_id
+FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
