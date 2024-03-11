cd2cfd8bb6fe5193b1302d0ce8577d8beabd0552
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-03-11 15:09:30 -0700

    i do NOT like C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,6 +5,6 @@ const x = parseInt(count[2]);
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x; i++) {
+  for (let i = 0; i < x; i++;) {
     console.log('C is fun');}
 }
