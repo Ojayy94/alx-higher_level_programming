5ec25d5285358cc91b567b3c469aaa46395d5efc
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-03-17 16:19:23 -0700

    script that lists all State objects from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """A python gile that contains a class definition and an instance Base"""
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base, State
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class City(Base):
@@ -24,4 +26,4 @@ class City(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, foreign_key("states.id"), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
