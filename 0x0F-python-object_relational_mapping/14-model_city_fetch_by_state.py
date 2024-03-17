95b8b752291c31830cc14044c3e3b364cf44e708
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-03-17 16:01:08 -0700

    script that lists all State objects from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """A script that lists all states from the database hbtn_0e_0_usa"""
 
-from model_state import Base, State
+from model_state import State
 from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
