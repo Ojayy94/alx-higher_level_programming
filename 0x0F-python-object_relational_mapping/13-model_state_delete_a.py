af19cca89cc30dd6ee32b4574282b223681ba7ae
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-03-17 15:45:30 -0700

    script that lists all State objects from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     session = Session()
 
     for state in session.query(State):
-        if 'a' in state.name:
+        if 'a' in State.name:
             session.delete(state)
     session.commit()
     session.close()
