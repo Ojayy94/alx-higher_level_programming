1675fbb19c127021f0c6f1fbdcd4bb1804fe7963
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-03-17 13:52:48 -0700

    script that lists all cities from states with SQL injection

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""A script that lists all cities from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    """our script should take 3 arguments: mysql username, mysql
+    password and database name (no argument validation needed)
+
+    You must use the module MySQLdb (import MySQLdb)
+
+    Your script should connect to a MySQL server running on
+    localhost at port 3306
+
+    Results must be sorted in ascending order by cities.id
+    """
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    script = db.cursor()
+    script.execute("SELECT * FROM cities \
+                 INNER JOIN states \
+                 ON cities.state_id = states.id \
+                 ORDER BY cities.id ASC")
+    show = script.fetchall()
+
+    for shows in show:
+        if shows[1] == argv[4]:
+            print(', '.join(shows))
