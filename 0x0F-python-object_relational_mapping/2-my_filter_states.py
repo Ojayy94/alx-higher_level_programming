9d5f0898a863b478f7fc4b8f638810026fae0f86
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-03-17 13:14:35 -0700

    script that lists all states

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
 
     script = db.cursor()
     script.execute("SELECT * FROM states \
-                 WHERE name = {} \
+                 WHERE BINARY name = '{}' \
                  ORDER BY states.id ASC".format(argv[4]))
     show = script.fetchall()
 
