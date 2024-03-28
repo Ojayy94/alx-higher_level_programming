249c91a60c34453a3181ef0242d1648d9d75bdfa
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-03-28 03:05:05 -0700

    advanced

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This program sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -s -o /dev/null -w '%{http_code}' "$1"
+curl -s "$1" -X POST -H "Content-Type: application/json" -d "$(cat "$2")"
