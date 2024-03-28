08ce5bd6900dc16631ed3b111042630a98f3a7c6
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-03-28 02:32:16 -0700

    network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-# YDisplay only body of a 200 status code response
-# You have to use curl
-
 curl -sL "$1" -X GET -D ./header -o ./output; if grep -q "200 OK" ./header; then cat ./output; fi
