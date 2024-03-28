c0ffa002cd757092e2e4e2baf295458217b0dcc3
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-03-28 02:27:54 -0700

    network

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
+# Bash script that sends a DELETE request to the URL passed as the first
+# +argument and displays the body of the response
 # You have to use curl
 
 curl -sX DELETE "$1"
