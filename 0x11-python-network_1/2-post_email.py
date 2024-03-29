a27fc5890c4947ba7d087c987324de789f1c534a
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-03-29 01:55:29 -0700

    Python script that displays url header response

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL and an email, sends a POST request to the
+   passed URL with the email as a parameter, and displays
+   the body of the response (decoded in utf-8)
+   """
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+import sys
+
+
+if __name__ == "__main__":
+    data = urlencode({
+                        "email": argv[2]
+                    }).encode("ascii")
+    req = Request(argv[1], data)
+
+    with urlopen(req) as res:
+        print(res.read().decode("utf-8"))
