87f62b6cb421b33da8ac55f70ef62c140e0d725e
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-03-29 01:58:34 -0700

    Python script that displays url email as a response

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,9 +10,9 @@ import sys
 
 if __name__ == "__main__":
     data = urlencode({
-                        "email": argv[2]
+                        "email": sys.argv[2]
                     }).encode("ascii")
-    req = Request(argv[1], data)
+    req = Request(sys.argv[1], data)
 
     with urlopen(req) as res:
         print(res.read().decode("utf-8"))
