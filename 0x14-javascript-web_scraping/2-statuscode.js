98dc5a929dba8d845eb39e7f1f3084c441b3b660
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-04-23 08:32:01 -0700

    web-scrapping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const request = require('request');
-request.get(process.argv[2].on('response', function (response) {
-  console.log(`code: ${response.statusCode}`);
+request(process.argv[2], function (error, response) {
+  console.log('code:', response.statusCode);
 });
