ba1450cd98b38b4557fdc916fdfc604447f27f2f
Oluwatosin Adebayo <ayojoshua01@gmail.com>
2024-04-23 14:54:21 -0700

    web-scrapping

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,9 +5,9 @@ const request = require('request');
 const fs = require('fs');
 
 request(process.argv[2], function (error, response, body) {
-  fc.writeFile(process.argv[3], body, 'utf-8', function (error) {
+  fs.writeFile(process.argv[3], body, 'utf-8', function (error) {
     if (error) {
       console.log(error);
     }
-});
+  });
 });
